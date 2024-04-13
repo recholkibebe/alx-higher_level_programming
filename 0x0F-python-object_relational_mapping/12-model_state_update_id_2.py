14a6b8020df9978b96bce809bfefdb95932ea29b
‘recholkibebe’ <recholkibebe@gmail.com>
2024-04-13 15:53:18 -0700

    more cities and files

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,33 +1,19 @@
 #!/usr/bin/python3
-"""
-This script deletes all State objects
-with a name containing the letter `a`
-from the database `hbtn_0e_6_usa`.
-"""
-
-from sys import argv
-from model_state import State, Base
+"""Script that changes the name of a State object from the
+database hbtn_0e_6_usa"""
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 if __name__ == "__main__":
-    """
-    Deletes State objects on the database.
-    """
-
-    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        argv[1], argv[2], argv[3])
-
-    engine = create_engine(db_url)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-
     session = Session()
-
-    states = session.query(State).filter(State.name.contains('a'))
-    if states is not None:
-        for state in states:
-            session.delete(state)
-
+    state = session.query(State).filter_by(id=2).first()
+    state.name = 'New Mexico'
     session.commit()
-
     session.close()
