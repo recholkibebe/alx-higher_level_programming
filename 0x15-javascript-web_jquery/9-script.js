16d7db404f47bd5d66bce1058fc1767ecfe3125b
recholkibebe <recholkibebe@gmail.com>
2024-05-30 11:22:38 -0700

    java query more

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,10 @@
+$(function () {
+  const $lists = $('DIV#hello');
+  $.ajax({
+    type: 'GET',
+    url: 'https://fourtonfish.com/hellosalut/?lang=fr',
+    success: function (data) {
+      $lists.text(data.hello);
+    }
+  });
+});
